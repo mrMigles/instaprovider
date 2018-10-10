dca196b2c1f9b30869529b7a1dabdb12c99dc10a
Sergei I. Ivanov <sergei.i.ivanov@netcracker.com>
2018-10-10 13:14:50 +0400

    update

--- main.go
+++ main.go
@@ -8,21 +8,23 @@ import (
 	"github.com/gorilla/mux"
 	"github.com/siongui/instago"
 	"golang.org/x/sync/errgroup"
-	"gopkg.in/ahmdrz/goinsta.v2"
 	"log"
 	"net/http"
 	"os"
+	"strconv"
 )
 
-type UserPhoto struct {
-	Description string
-	URL         string
-	Likes       int
+type InstaPost struct {
+	Description string `json:"description"`
+	PhotoURL    string `json:"photo_url"`
+	Likes       int64  `json:"likes"`
+	ID          string `json:"id"`
+	PostUrl     string `json:"post_url"`
 }
 
 type InstaUser struct {
-	UserName string
-	Photos   []UserPhoto
+	UserName string      `json:"user_name"`
+	Posts    []InstaPost `json:"posts"`
 }
 
 var (
@@ -30,24 +32,23 @@ var (
 		"Host to serve requests incoming to Streaming Configurator")
 	servePort = flag.String("serve_port", getEnv("SERVER_PORT", "8080"),
 		"Port to serve requests incoming to Streaming Configurator")
-	g   errgroup.Group
-	mgr instago.IGApiManager
+
+	dcUserId  = flag.String("dc_user_id", getEnv("DC_USER_ID", ""), "")
+	sessionID = flag.String("session_id", getEnv("SESSION_ID", ""), "")
+	csrfToken = flag.String("csrf_token", getEnv("CSRF_TOKEN", ""), "")
+	g         errgroup.Group
+	mgr       instago.IGApiManager
 )
 
 func main() {
-	mgr = *instago.NewInstagramApiManager("IG_DS_USER_ID", "IG_SESSIONID", "IG_CSRFTOKEN")
-	medias, _ := mgr.GetAllPostMedia("sergeyivanov93")
+	mgr = *instago.NewInstagramApiManager(*dcUserId, *sessionID, *csrfToken)
+	medias, _ := mgr.GetAllPostMedia("nc_ficus")
 	log.Print(medias[0].DisplayUrl)
 
 	mainEndpoints := &http.Server{
 		Addr:    fmt.Sprintf("%s:%s", *serveHost, *servePort),
-		Handler: RemindHandler(),
+		Handler: handler(),
 	}
-
-	insta := goinsta.New("USERNAME", "PASSWORD")
-	insta.Login()
-	user, _ := insta.Search.User("nc_ficus")
-	log.Print(user.Users[0].Feed().Items[0].Likes)
 
 	g.Go(func() error {
 		return mainEndpoints.ListenAndServe()
@@ -66,31 +67,59 @@ func getEnv(key, fallback string) string {
 	return fallback
 }
 
-func RemindHandler() http.Handler {
+func handler() http.Handler {
 	r := mux.NewRouter()
 	handler := Handler()
+	r.Handle("/api/insta/{username}/{last}",
+		handlers.LoggingHandler(
+			os.Stdout,
+			handler(handleFunc())),
+	).Methods("GET")
 	r.Handle("/api/insta/{username}",
 		handlers.LoggingHandler(
 			os.Stdout,
-			handler(HandleRemind())),
+			handler(handleFunc())),
 	).Methods("GET")
 
 	return JsonContentType(handlers.CompressHandler(r))
 }
 
-func HandleRemind() func(w http.ResponseWriter, r *http.Request) {
+func handleFunc() func(w http.ResponseWriter, r *http.Request) {
 	return func(w http.ResponseWriter, r *http.Request) {
 		vars := mux.Vars(r)
 		name := vars["username"]
+		id := getInt(vars["last"], 0)
 		medias, _ := mgr.GetAllPostMedia(name)
-		resp := &InstaUser{UserName: name, Photos: []UserPhoto{}}
+		resp := &InstaUser{UserName: name, Posts: []InstaPost{}}
 		for _, media := range medias {
-			resp.Photos = append(resp.Photos, UserPhoto{Description: media.Typename, URL: media.DisplayUrl})
+			mediaId := getInt(media.Id, 0)
+			if mediaId <= id {
+				break
+			}
+			postInfo := InstaPost{
+				PhotoURL: media.DisplayUrl,
+				PostUrl:  media.GetPostUrl(),
+				Likes:    media.EdgeMediaPreviewLike.Count,
+				ID:       media.Id,
+			}
+			if len(media.EdgeMediaToCaption.Edges) > 0 {
+				postInfo.Description = media.EdgeMediaToCaption.Edges[0].Node.Text
+			}
+			resp.Posts = append(resp.Posts, postInfo)
 		}
 		res, _ := json.Marshal(resp)
 		w.WriteHeader(200)
 		w.Write(res)
 	}
+}
+
+func getInt(strValue string, defaultValue int64) int64 {
+	intValue, err := strconv.ParseInt(strValue, 10, 64)
+	if err != nil {
+		fmt.Printf("Incorrect int value, default value %+v will be used ", defaultValue)
+		return defaultValue
+	}
+	return intValue
 }
 
 func Handler() func(func(w http.ResponseWriter, r *http.Request)) http.Handler {
